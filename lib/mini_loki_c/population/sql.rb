78f5b9bc80b7b35fccc19aabb266a717125d6199
evilx1990 <evilx.emelyanov@gmail.com>
2020-06-22 16:22:29 +0300

    deploy

--- lib/mini_loki_c/population/sql.rb
+++ lib/mini_loki_c/population/sql.rb
@@ -2,16 +2,27 @@
 
 module MiniLokiC
   module Population
+    # module generate SQL queries
     module SQL
       module_function
 
       def insert_on_duplicate_key(table, options = {})
-        keys = options.keys.map { |k| "`#{k}`" }.join(', ')
-        values = options.values.map { |v| v.nil? ? 'null' : v.to_s.to_json }.join(', ')
-        updates = options.map { |(k, v)| "`#{k}` = #{v.nil? ? 'null' : v.to_s.to_json}" }.join(', ')
+        keys = []
+        values = []
+        updates = []
 
-        "insert into `#{table}` (#{keys}) values (#{values}) "\
-        "on duplicate key update #{updates}"
+        options.map do |(k, v)|
+          key = "`#{k}`"
+          value = v.nil? ? 'null' : v.to_s.to_json
+
+          keys << key
+          values << value
+          updates << "#{key} = #{value}"
+        end
+
+        "INSERT INTO `#{table}` (#{keys.join(', ')}) "\
+        "VALUES (#{values.join(', ')}) "\
+        "ON DUPLICATE KEY UPDATE #{updates.join(', ')}"
       end
     end
   end
