dd92e4f32870ce7935ae6f644193ee075839623b
evilx1990 <evilx.emelyanov@gmail.com>
2020-07-01 17:32:53 +0300

    develop

--- app/helpers/iterations_helper.rb
+++ app/helpers/iterations_helper.rb
@@ -1,18 +1,4 @@
 # frozen_string_literal: true
 
 module IterationsHelper
-  def iteration_color(story_type, iteration)
-    case iteration.statuses.first.name
-    when 'not started'
-      'secondary'
-    when 'in progress'
-      'primary'
-    when 'exported'
-      'dark'
-    when 'on cron'
-      'warning'
-    when 'blocked'
-      'danger'
-    end
-  end
 end
