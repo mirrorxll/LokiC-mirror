afdff53e72b4d320f9f87de0d17f9c4420769335
evilx1990 <evilx.emelyanov@gmail.com>
2020-07-27 20:59:28 +0300

    deploy

--- lib/mini_loki_c/population/publications/by_org_client_id.rb
+++ lib/mini_loki_c/population/publications/by_org_client_id.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
-require_relative 'by_client_id/query.rb'
+require_relative 'by_org_client_id/query.rb'
 
 module MiniLokiC
   module Population
     module Publications
       # get publications by client id
-      class ByClientId < Publications::Base
+      class ByOrgClientId < Publications::Base
         def initialize(org_id, client_ids = [])
           super()
           @client_ids = client_ids.join(',')
