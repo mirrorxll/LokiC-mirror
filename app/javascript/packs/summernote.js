27db1aaf119b8e43a6dd09059367e2d6c085955a
evilx1990 <evilx.emelyanov@gmail.com>
2020-07-30 12:18:46 +0300

    deploy

--- app/javascript/packs/summernote.js
+++ app/javascript/packs/summernote.js
@@ -5,21 +5,3 @@ require('codemirror/lib/codemirror.js');
 require('codemirror/lib/codemirror.css');
 require('codemirror/mode/xml/xml.js');
 require('codemirror/theme/monokai.css');
-
-localStorage.setItem('summernoteSettings', JSON.stringify({
-    height: 700,
-    fontSizes: ['8', '9', '10', '11', '12', '14', '18', '24', '30', '36', '48', '60', '72', '96'],
-    lineHeights: ['0.2', '0.3', '0.4', '0.5', '0.6', '0.8', '1.0', '1.2', '1.4', '1.5', '2.0', '3.0'],
-    toolbar: [
-        ['style', ['style']],
-        ['font', ['bold', 'underline', 'clear']],
-        ['fontname', ['fontname']],
-        ['fontsize', ['fontsize']],
-        ['color', ['color']],
-        ['para', ['ul', 'ol', 'paragraph']],
-        ['height', ['height']],
-        ['table', ['table']],
-        ['insert', ['link', 'picture', 'video']],
-        ['view', ['fullscreen', 'codeview', 'help']],
-    ]
-}));
