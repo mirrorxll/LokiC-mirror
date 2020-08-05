22e75c27df8f69048644fd29a3dbb300618c1747
evilx1990 <evilx.emelyanov@gmail.com>
2020-08-05 16:59:13 +0300

    deploy

--- app/jobs/schedule_counts_job.rb
+++
@@ -1,4 +0,0 @@
-
-
-hash = {}
-StoryType.last.iteration.samples.select("publication_id, count(publication_id) count").group('publication_id').group_by { |el| el.client_name }.each { |k,v| hash[k] =  v.max_by{|el| el.count }.c ount }
