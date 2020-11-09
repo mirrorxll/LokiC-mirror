e04099e58994cee11fdfca590933b1e4b0598fa4
evilx1990 <evilx.emelyanov@gmail.com>
2020-11-09 17:53:02 +0200

    deploy

--- db/primary_migrate/20200421202030_create_export_configurations.rb
+++ db/primary_migrate/20200421202030_create_export_configurations.rb
@@ -2,14 +2,15 @@
 
 class CreateExportConfigurations < ActiveRecord::Migration[6.0]
   def change
+    env = %w[development test].include?(Rails.env) ? 'staging' : Rails.env
+
     create_table :export_configurations do |t|
       t.belongs_to :story_type
       t.belongs_to :publication
       t.belongs_to :tag
       t.belongs_to :photo_bucket
 
-      t.integer :production_job_item
-      t.integer :staging_job_item
+      t.integer "#{env}_job_item".to_sym
       t.boolean :skipped, default: false
       t.timestamps
     end
