e514176afb7f5ae1dc398305969c150a969c9d7b
evilx1990 <evilx.emelyanov@gmail.com>
2020-11-17 22:17:27 +0200

    deploy

--- app/jobs/samples_and_auto_feedback_job.rb
+++ app/jobs/samples_and_auto_feedback_job.rb
@@ -9,9 +9,11 @@ class SamplesAndAutoFeedbackJob < ApplicationJob
     sample_args = { columns: column_names, ids: params[:row_ids] }
     edge_ids = Table.select_edge_ids(staging_table.name, column_names)
     row_ids = params[:row_ids].delete(' ').split(',')
-    ids = (edge_ids + row_ids).join(',')
+    ids = (edge_ids + row_ids)
 
-    MiniLokiC::Code.execute(story_type, :creation, sampled: true, ids: ids)
+    MiniLokiC::Code.execute(story_type, :creation, sampled: true, ids: ids.join(','))
+    story_type.iteration.samples.where(staging_row_id: ids, sampled: false).update_all(sampled: true)
+
     Samples.auto_feedback(story_type)
     status = true
     message = 'samples created.'
