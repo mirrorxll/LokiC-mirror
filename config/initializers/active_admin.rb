7be8c23b01454a0955ed7b4b055eda77705ae443
evilx1990 <evilx.emelyanov@gmail.com>
2020-11-23 14:53:03 +0200

    develop

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -1,13 +1,13 @@
+# frozen_string_literal: true
+
 ActiveAdmin.setup do |config|
   config.use_webpacker = true
   config.skip_before_action :find_parent_story_type
   config.skip_before_action :set_iteration
   # == Site Title
-  #
   # Set the title that is displayed on the main layout
   # for each of the active admin pages.
-  #
-  config.site_title = "Loki C"
+  config.site_title = 'LokiC'
 
   # Set the link url for the title. For example, to take
   # users to your main site. Defaults to no link.
