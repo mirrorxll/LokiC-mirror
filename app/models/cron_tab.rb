9f3c5ea2440e14ea87e1c14d5cb6b7099a849437
evilx1990 <evilx.emelyanov@gmail.com>
2021-02-03 15:25:30 +0200

    move branch to other pc

--- app/models/cron_tab.rb
+++ app/models/cron_tab.rb
@@ -4,4 +4,8 @@ class CronTab < ApplicationRecord
   serialize :setup, Hash
 
   belongs_to :story_type
+
+  def pattern
+    "#{setup[:minute]} #{setup[:hour]} #{setup[:day]} #{setup[:minute]} #{setup[:minute]} "
+  end
 end
