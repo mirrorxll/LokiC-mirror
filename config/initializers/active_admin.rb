968436b962b71c7b42ea183d48423273f71d4626
dmitriybuzina <dmitry.buzina@faceit-team.com>
2021-03-12 06:17:47 +0200

    admin panel

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -286,7 +286,7 @@ ActiveAdmin.setup do |config|
   # hand side with a filter for each attribute of the registered model.
   # You can enable or disable them for all resources here.
   #
-  # config.filters = true
+  config.filters = false
   #
   # By default the filters include associations in a select, which means
   # that every record will be loaded for each association (up
@@ -294,7 +294,7 @@ ActiveAdmin.setup do |config|
   # You can enabled or disable the inclusion
   # of those filters by default here.
   #
-  # config.include_default_association_filters = true
+  config.include_default_association_filters = false
 
   # config.maximum_association_filter_arity = 256 # default value of :unlimited will change to 256 in a future version
   # config.filter_columns_for_large_association, [
