25f3a397a8494aaaf35e8fc4740b8eb7955d6b55
evilx1990 <evilx.emelyanov@gmail.com>
2021-05-20 19:26:13 +0300

    deploy

--- db/secondary_migrate/20210518142444_add_story_type_id_to_exported_story_types.rb
+++ db/secondary_migrate/20210518142444_add_story_type_id_to_exported_story_types.rb
@@ -2,10 +2,11 @@
 
 class AddStoryTypeIdToExportedStoryTypes < ActiveRecord::Migration[6.0]
   def change
-    add_column :exported_story_types, :story_type_id, :integer,
-               limit: 8, after: :developer_id
-    add_index :exported_story_types, :story_type_id
 
-    ExportedStoryType.all.each { |est| est.update(story_type: est.iteration.story_type) }
+    # add_column :exported_story_types, :story_type_id, :integer,
+    #            limit: 8, after: :developer_id
+    # add_index :exported_story_types, :story_type_id
+
+    # ExportedStoryType.all.each { |est| est.update(story_type: est.iteration.story_type) }
   end
 end
