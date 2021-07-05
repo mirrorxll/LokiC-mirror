e49b9f0a42dd102aead408fbfcd825c57b764370
hongodor <detectivekunkun@2ch.hk>
2021-07-05 22:24:37 +0800

    fixed gather task link

--- db/primary_migrate/20210702144210_add_gather_task_to_story_type.rb
+++ db/primary_migrate/20210702144210_add_gather_task_to_story_type.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class AddGatherTaskIdToStoryType < ActiveRecord::Migration[6.0]
+class AddGatherTaskToStoryType < ActiveRecord::Migration[6.0]
   def change
     add_column :story_types, :gather_task, :integer, after: :comment
   end
