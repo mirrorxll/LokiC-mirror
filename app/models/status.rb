e422851897c4f007d6633f95aefdd537f0268ae8
rubyxgenius <sergey.rubygenius@gmail.com>
2021-08-30 23:30:19 +0300

    deploy

--- app/models/status.rb
+++ app/models/status.rb
@@ -4,15 +4,15 @@ class Status < ApplicationRecord
   has_many :story_types
 
   def self.story_type_dev_statuses
-    where.not(name: ['not started', 'migrated', 'inactive', 'deleted'])
+    where.not(name: ['not started', 'migrated', 'inactive', 'deleted', 'on checking'])
   end
 
   def self.article_type_dev_statuses
-    where.not(name: ['not started', 'inactive', 'deleted'])
+    where.not(name: ['not started', 'inactive', 'deleted', 'on checking'])
   end
 
   def self.scrape_task_dev_statuses
-    where.not(name: ['not started', 'migrated', 'inactive', 'exported', 'on cron', 'deleted'])
+    where.not(name: ['not started', 'migrated', 'inactive', 'exported', 'on cron', 'deleted', 'on checking'])
   end
 
   def self.multi_task_dev_statuses
