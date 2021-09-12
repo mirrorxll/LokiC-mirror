2eed3cdaadca16fc50424ee78e21e445a2f9305b
rubyxgenius <sergey.rubygenius@gmail.com>
2021-09-12 16:51:26 +0300

    develop

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,3 +1,11 @@
 # frozen_string_literal: true
 
 Redis.exists_returns_integer = false
+
+Sidekiq.configure_client do |config|
+  config.redis = { url: 'redis://127.0.0.1:6379/1' }
+end
+
+Sidekiq.configure_server do |config|
+  config.redis = { url: 'redis://127.0.0.1:6379/1' }
+end
