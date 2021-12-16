96fd2b3506b2c6b38236da9d883fe81310f6403d
dmitriybuzina <dmitry.buzina@faceit-team.com>
2021-12-16 23:44:45 +0200

    fixed

--- db/primary_migrate/20210921123951_add_columns_to_task_assignments.rb
+++ db/primary_migrate/20210921123951_add_columns_to_task_assignments.rb
@@ -7,10 +7,6 @@ class AddColumnsToTaskAssignments < ActiveRecord::Migration[6.0]
       t.datetime :confirmed_at, after: :confirmed
       t.decimal  :hours, scale: 2, precision: 6, after: :confirmed_at
       t.boolean  :done, default: false, after: :hours
-
-      TaskAssignment.all.each do |task_assignment|
-        task_assignment.update!(confirmed_at: task_assignment[:created_at].to_s, confirmed: true)
-      end
     end
   end
 end
