e7152310fae44aa05d6ecb06d8909a128fa209d1
dmitriybuzina <dmitry.buzina@faceit-team.com>
2021-12-20 20:38:11 +0200

    fixed

--- app/models/task_team_work.rb
+++ app/models/task_team_work.rb
@@ -3,4 +3,8 @@
 class TaskTeamWork < ApplicationRecord # :nodoc:
   belongs_to :task
   belongs_to :creator, class_name: 'Account'
+
+  def self.done
+    joins(:task).where('status_id=?', Status.find_by(name: 'done'))
+  end
 end
