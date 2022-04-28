b0fbe321e6b7b74b07238d8e58129b0770b8ec55
magas4891 <magasishe@gmail.com>
2022-04-28 13:09:56 +0300

    updated query

--- app/tasks/story_types/iterations/set_max_time_frame_task.rb
+++ app/tasks/story_types/iterations/set_max_time_frame_task.rb
@@ -5,9 +5,13 @@ module StoryTypes
     class SetMaxTimeFrameTask < StoryTypesTask
       def perform(story_type_id)
         story_type    = StoryType.find(story_type_id)
-        stories       = story_type.stories.exported
-        time_frames   = stories.joins(:time_frame).pluck('time_frames.frame')
-        sorted_frames = time_frames.sort_by { |frame| [frame.split(':')[-1].to_i, frame.split(':')[1].to_i] }
+        sorted_frames = StoryType.select('time_frames.frame as frame')
+                                 .joins(stories: :time_frame)
+                                 .where(id: story_type_id)
+                                 .where.not("stories.pl_#{PL_TARGET}_story_id" => nil)
+                                 .distinct
+                                 .map(&:frame)
+                                 .sort_by { |frame| [frame.split(':')[-1].to_i, frame.split(':')[1].to_i] }
 
         story_type.update!(max_time_frame: sorted_frames.last)
       end
