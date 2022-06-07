6a073289d553522be80d403dabeb77a34fa8b60a
magas4891 <magasishe@gmail.com>
2022-06-07 09:49:05 +0300

    updated JS functions

--- app/javascript/packs/factoids/purge.js
+++ app/javascript/packs/factoids/purge.js
@@ -12,35 +12,33 @@ $(document).on('change', ("input[type='checkbox']"), function() {
 $(document).on('click', ('button#select_all_button'), function() {
     var disabled_button = $('button#delete_selected');
     var trigger = $('input#trigger');
-    if (trigger.val()) {
-        console.log('------------------');
+    if (trigger.val() == 'true') {
         $("input[type='checkbox']").each(function() {
             this.checked = false;
         });
         this.textContent = 'Select All';
-        trigger.value = false;
+        disabled_button.attr('disabled', true);
+        trigger.val(false);
     } else {
-        console.log('+++++++++++++++++');
-
         $("input[type='checkbox']").each(function() {
             this.checked = true;
         });
-        this.textContent = 'Deselect All';
+        this.textContent = 'Undo Select';
         disabled_button.attr('disabled', false);
-        trigger.value = true;
+        trigger.val(true);
     }
 });
 
 $(document).on('click', ('button#delete_selected'), function() {
     if (!confirm('Are you sure?')) return;
 
-    var values = [];
+    var factoids = [];
     var iteration_id = $('#iteration_exported_factoids').attr('iteration_exported_factoids_id');
     var article_types_id = $('#iteration_exported_factoids').attr('article_type_id');
 
     document.querySelectorAll("input[type='checkbox']").forEach(elem => {
         if (elem.checked) {
-            values.push(elem.id)
+            factoids.push(elem.id)
         }
     });
 
@@ -48,6 +46,6 @@ $(document).on('click', ('button#delete_selected'), function() {
         type: 'DELETE',
         dataType: 'script',
         url: `${window.location.origin}/article_types/${article_types_id}/iterations/${iteration_id}/export/remove_selected_factoids`,
-        data: `factoids_ids=${values}`
+        data: `factoids_ids=${factoids}`
     })
 });
