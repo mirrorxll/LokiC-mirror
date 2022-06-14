b6e125d89851498bcc3b55e4ab1d8da62551083d
magas4891 <magasishe@gmail.com>
2022-06-14 17:31:40 +0300

    done with JS

--- app/javascript/packs/story_type/properties_form.js
+++ app/javascript/packs/story_type/properties_form.js
@@ -1,4 +1,4 @@
-$(document).on('click', 'button#fill_clients', function() {
+$(document).on('click', 'input#update_opportunities', function() {
     let story_type_id = $('#story_type').attr('story_type_id');
     let oppo_id = $('#default_opportunity_opportunity_id').val();
     let oppo_type_id = $('#default_opportunity_opportunity_type_id').val();
@@ -14,4 +14,26 @@ $(document).on('click', 'button#fill_clients', function() {
         url: `/story_types/${story_type_id}/default_opportunities/update_opportunities`,
         data: params
     })
+});
+
+function enableDisabledButton() {
+    var opportunity = $('#default_opportunity_opportunity_id').val();
+    console.log(opportunity);
+    var opportunity_type = $('#default_opportunity_opportunity_type_id').val();
+    console.log(opportunity_type);
+    var content_type = $('#default_opportunity_content_type_id').val();
+    console.log(content_type);
+    if (opportunity.length > 0 && opportunity_type.length > 0 && content_type.length > 0) {
+        $('#update_opportunities').attr('disabled', false);
+    } else {
+        $('#update_opportunities').attr('disabled', true);
+    }
+}
+
+$(document).on('shown.bs.modal', '#storyTypePropertiesForm', function () {
+    enableDisabledButton();
+});
+
+$(document).on('change', '#default_opportunity_opportunity_id, #default_opportunity_opportunity_type_id, #default_opportunity_content_type_id', function() {
+    enableDisabledButton();
 });
