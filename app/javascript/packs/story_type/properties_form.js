078af811f1c9bc84949db09a995dc0f52d5b3d6e
magas4891 <magasishe@gmail.com>
2022-06-15 09:34:53 +0300

    fixed some typos

--- app/javascript/packs/story_type/properties_form.js
+++ app/javascript/packs/story_type/properties_form.js
@@ -9,26 +9,12 @@ $(document).on('click', 'input#update_opportunities', function() {
                  `update_opportunities[content_type_id]=${content_type_id}`
 
     Rails.ajax({
-        type: "PATCH",
+        type: 'PATCH',
         dataType: 'json',
         url: `/story_types/${story_type_id}/default_opportunities/update_opportunities`,
         data: params
     })
 });
-
-function enableDisabledButton() {
-    var opportunity = $('#default_opportunity_opportunity_id').val();
-    console.log(opportunity);
-    var opportunity_type = $('#default_opportunity_opportunity_type_id').val();
-    console.log(opportunity_type);
-    var content_type = $('#default_opportunity_content_type_id').val();
-    console.log(content_type);
-    if (opportunity.length > 0 && opportunity_type.length > 0 && content_type.length > 0) {
-        $('#update_opportunities').attr('disabled', false);
-    } else {
-        $('#update_opportunities').attr('disabled', true);
-    }
-}
 
 $(document).on('shown.bs.modal', '#storyTypePropertiesForm', function () {
     enableDisabledButton();
@@ -37,3 +23,23 @@ $(document).on('shown.bs.modal', '#storyTypePropertiesForm', function () {
 $(document).on('change', '#default_opportunity_opportunity_id, #default_opportunity_opportunity_type_id, #default_opportunity_content_type_id', function() {
     enableDisabledButton();
 });
+
+$(document).on('click', "input[value='update opportunity list']", function() {
+    enableDisabledButton();
+});
+
+$(window).on('load ajaxStop', function() {
+    enableDisabledButton();
+});
+
+function enableDisabledButton() {
+    let opportunity = $('#default_opportunity_opportunity_id').val();
+    let opportunity_type = $('#default_opportunity_opportunity_type_id').val();
+    let content_type = $('#default_opportunity_content_type_id').val();
+
+    if (opportunity && opportunity.length > 0 && opportunity_type.length > 0 && content_type.length > 0) {
+        $('#update_opportunities').attr('disabled', false);
+    } else {
+        $('#update_opportunities').attr('disabled', true);
+    }
+}
