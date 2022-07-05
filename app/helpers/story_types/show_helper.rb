f42e60451011a49e013050cd6028415782a8eef6
magas4891 <magasishe@gmail.com>
2022-07-05 18:41:57 +0300

    done with export section

---
+++ app/helpers/story_types/show_helper.rb
@@ -0,0 +1,15 @@
+module StoryTypes::ShowHelper
+  def purge_export_availability
+    stories = @iteration.stories.where(backdated: false)
+    if @story_type.staging_table && @iteration.creation && @iteration.schedule &&
+      if @iteration.export.eql?(true) && !@iteration.purge_export.eql?(true)
+        story = stories.where.not(published_at: nil).order(:published_at).first
+        return true unless story && story.published_at <= Time.now
+      else
+        exported = stories.where.not("pl_#{PL_TARGET}_story_id".to_sym => nil)
+        return true if exported.count > 0
+      end
+    end
+  end
+end
+
