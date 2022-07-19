3251e2f4ed9f632431f950ff256fb2221c21b786
evilxgenius <evilx.emelyanov@gmail.com>
2022-07-19 14:44:37 +0300

    develop

--- app/controllers/scrape_tasks/data_sets_controller.rb
+++ app/controllers/scrape_tasks/data_sets_controller.rb
@@ -11,8 +11,8 @@ module ScrapeTasks
     def update
       @scrape_task.data_sets.clear
 
-      multi_tasks_params.keep_if { |_k, v| v.eql?('1') }.keys.each do |t|
-        @scrape_task.multi_tasks << Task.find(t)
+      data_sets_params.keep_if { |_k, v| v.eql?('1') }.keys.each do |ds|
+        @scrape_task.data_sets << DataSet.find(ds)
       end
 
       render 'scrape_tasks/data_sets/show'
@@ -20,7 +20,7 @@ module ScrapeTasks
 
     private
 
-    def multi_tasks_params
+    def data_sets_params
       params[:data_sets] ? params.require(:data_sets).permit!.to_h : {}
     end
   end
