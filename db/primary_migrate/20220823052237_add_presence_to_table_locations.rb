cb20e973fe66dda0044ee62c6326c375aaef7219
magas4891 <magasishe@gmail.com>
2022-08-25 19:20:20 +0300

    updated text and migration

--- db/primary_migrate/20220823052237_add_presence_to_table_locations.rb
+++ db/primary_migrate/20220823052237_add_presence_to_table_locations.rb
@@ -1,5 +1,5 @@
 class AddPresenceToTableLocations < ActiveRecord::Migration[6.0]
   def change
-    add_column :table_locations, :presence, :boolean, after: :verified
+    add_column :table_locations, :presence, :boolean, default: true, after: :verified
   end
 end
