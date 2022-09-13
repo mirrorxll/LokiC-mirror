8e58da85a264562a4df2c485548fe4bedcb3a5db
magas4891 <magasishe@gmail.com>
2022-09-13 16:36:16 +0300

    updated helper

--- app/javascript/packs/common.js
+++ app/javascript/packs/common.js
@@ -21,3 +21,19 @@ $(document).on('change', ('form#progress_status'), function() {
         data: `status_id=${statusId}` + (reason ? `&reason=${reason}` : '')
     });
 });
+
+// editing comment of story_type or factoid_type
+$(document).on('show.bs.collapse', ('#commentForm'), function() {
+    $("textarea[id$='_type_comment']") .froalaEditor({
+        key: 'KfdolbcqsaA2wzA-13==',
+        editorClass: 'border rounded p-2',
+        toolbarInline: true,
+        toolbarButtons: ['undo', 'redo', '|', 'bold', 'italic', 'underline', 'strikeThrough', '|', 'color', 'lineHeight', '|', 'paragraphFormat', 'align', 'formatOL', 'formatUL', 'insertLink', 'insertImage'],
+        imageUploadURL: '/images/upload',
+        imageUploadMethod: 'POST',
+        toolbarVisibleWithoutSelection: false,
+        zIndex: 3333,
+        heightMin: 100,
+        placeholderText: 'Type here....'
+    })
+});
