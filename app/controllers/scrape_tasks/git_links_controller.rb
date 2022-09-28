1d8685bffe97a2dd83c7c9490b6fd6f728df6292
Sergey E <sergey.rubygenius@gmail.com>
2022-09-28 17:44:35 +0300

    develop

--- app/controllers/scrape_tasks/git_links_controller.rb
+++ app/controllers/scrape_tasks/git_links_controller.rb
@@ -9,18 +9,17 @@ module ScrapeTasks
     def edit; end
 
     def update
-      ScrapeTaskGitLink.find_or_create_by(scrape_task_id: @scrape_task.id).update(git_params)
       @git_link = ScrapeTaskGitLink.find_or_create_by(scrape_task_id: @scrape_task.id)
       @git_link.update(git_params)
 
-      flash.now[:error] = @git_link.errors if @git_link.errors.messages.any?
+      flash.now[:error] = { git_link: @git_link.errors.full_messages } if @git_link.errors.any?
       render 'scrape_tasks/git_links/show'
     end
 
     private
 
     def git_params
-       params.require(:git_link).permit(:link)
+      params.require(:git_link).permit(:link)
     end
   end
 end
