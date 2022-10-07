a16de734f74423a334c743d6704c502bc879d2f2
Sergey E <sergey.rubygenius@gmail.com>
2022-10-07 20:47:09 +0300

    develop

--- lib/mini_loki_c/population/article_type_population_success.rb
+++ lib/mini_loki_c/population/article_type_population_success.rb
@@ -3,7 +3,7 @@
 module MiniLokiC
   module Population
     module ArticleTypePopulationSuccess
-      def self.[](staging_table, options); end
+      def self.[](staging_table, options = {}); end
     end
   end
 end
