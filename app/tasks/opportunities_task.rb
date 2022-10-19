13937e994956cbf7224addfe320bac9932549050
Sergey E <sergey.rubygenius@gmail.com>
2022-10-20 01:41:19 +0300

    develop

--- app/tasks/opportunities_task.rb
+++ app/tasks/opportunities_task.rb
@@ -4,13 +4,15 @@ class OpportunitiesTask < ApplicationTask
   def perform(*_args)
     replica = PipelineReplica[:production]
 
+    agency_ids = []
     replica.get_agencies.each do |row|
       agency = Agency.find_or_create_by!(id: row['id'])
       agency.update!(name: row['name'], partner: row['partner'])
-      agency.touch
+      agency_ids << row['id']
     end
-    Agency.where('DATE(updated_at) < CURRENT_DATE()').destroy_all
+    Agency.where.not(id: agency_ids).destroy_all
 
+    opportunity_ids = []
     replica.get_opportunities.each do |row|
       opportunity = Opportunity.find_or_create_by!(id: row['id'])
       opportunity.update!(
@@ -18,17 +20,19 @@ class OpportunitiesTask < ApplicationTask
         archived_at: row['archived_at'],
         agency_id: row['agency_id']
       )
-      opportunity.touch
+      opportunity_ids << row['id']
     end
-    Opportunity.where('DATE(updated_at) < CURRENT_DATE()').destroy_all
+    Opportunity.where.not(id: opportunity_ids).destroy_all
 
+    opportunity_type_ids = []
     replica.get_opportunity_types.each do |row|
       opportunity_type = OpportunityType.find_or_create_by!(id: row['id'])
       opportunity_type.update!(name: row['name'])
-      opportunity_type.touch
+      opportunity_type_ids << row['id']
     end
-    OpportunityType.where('DATE(updated_at) < CURRENT_DATE()').destroy_all
+    OpportunityType.where.not(id: opportunity_type_ids).destroy_all
 
+    content_type_ids = []
     replica.get_content_types.each do |row|
       content_type = ContentType.find_or_create_by!(id: row['id'])
       content_type.update!(
@@ -36,10 +40,11 @@ class OpportunitiesTask < ApplicationTask
         archived_at: row['archived_at'],
         content_group_id: row['content_group_id']
       )
-      content_type.touch
+      content_type_ids << row['id']
     end
-    ContentType.where('DATE(updated_at) < CURRENT_DATE()').destroy_all
+    ContentType.where.not(id: content_type_ids).destroy_all
 
+    join_opp_type_ids = []
     replica.get_join_opportunity_types.each do |row|
       join_opp_type = OpportunityOpportunityType.find_or_create_by!(id: row['id'])
       join_opp_type.update!(
@@ -47,10 +52,11 @@ class OpportunitiesTask < ApplicationTask
         opportunity_type_id: row['opportunity_type_id'],
         archived_at: row['archived_at']
       )
-      join_opp_type.touch
+      join_opp_type_ids << row['id']
     end
-    OpportunityOpportunityType.where('DATE(updated_at) < CURRENT_DATE()').destroy_all
+    OpportunityOpportunityType.where.not(id: join_opp_type_ids).destroy_all
 
+    join_cont_type_ids = []
     replica.get_join_content_types.each do |row|
       join_cont_type = OpportunityContentType.find_or_create_by!(id: row['id'])
       join_cont_type.update!(
@@ -58,10 +64,11 @@ class OpportunitiesTask < ApplicationTask
         content_type_id: row['content_type_id'],
         archived_at: row['archived_at']
       )
-      join_cont_type.touch
+      join_cont_type_ids << row['id']
     end
-    OpportunityContentType.where('DATE(updated_at) < CURRENT_DATE()').destroy_all
+    OpportunityContentType.where.not(id: join_cont_type_ids).destroy_all
 
+    join_pubs_ids = []
     replica.get_join_publications.each do |row|
       join_pubs = OpportunityPublication.find_or_create_by!(id: row['id'])
       publication = Publication.find_by(pl_identifier: row['project_id'])
@@ -72,20 +79,20 @@ class OpportunitiesTask < ApplicationTask
         publication_id: publication.id,
         archived_at: row['archived_at']
       )
-      join_pubs.touch
+      join_pubs_ids << row['id']
     end
-    OpportunityPublication.where('DATE(updated_at) < CURRENT_DATE()').destroy_all
+    OpportunityPublication.where.not(id: join_pubs_ids).destroy_all
 
     replica.close
 
+    client_oppo_ids = []
     Client.all.each do |c|
       c.publications.each do |p|
         p.opportunities.each do |o|
-          ClientOpportunity.find_or_create_by!(client: c, opportunity: o).touch
+          client_oppo_ids << ClientOpportunity.find_or_create_by!(client: c, opportunity: o).id
         end
       end
     end
-
-    ClientOpportunity.where('DATE(updated_at) < CURRENT_DATE()').destroy_all
+    ClientOpportunity.where.not(id: client_oppo_ids).destroy_all
   end
 end
